1964d5389278153a6f06a2b27ac4051f040d4460
eMode18 <ewagandi@gmail.com>
2023-02-17 12:48:07 -0800

    More c tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main entry point
+ * return type int, value 0
+*/
+
+int main(void){
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main is the programs entry point
+ * main should return value of 0 type int
+*/
+
+int main(void) {
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Sizee of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float);
+	return (0);
