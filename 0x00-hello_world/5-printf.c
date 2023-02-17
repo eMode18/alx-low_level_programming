a0139f7bdfdd8f0d1e69d09698f2e4f8fc395184
eMode18 <ewagandi@gmail.com>
2023-02-17 13:44:52 -0800

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
 /**
-* main entry point
-* return type int, value 0
+* main - entry point
+*
+* Return 0 (success)
 */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
+
