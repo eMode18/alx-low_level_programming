4674cfc0c10859e55dabf2cee10f81d604caa561
eMode18 <ewagandi@gmail.com>
2023-02-17 23:55:50 -0800

    my second commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - begin exection here
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
