2a49984b630031d61b6009b234ca54951a7f797e
eMode18 <ewagandi@gmail.com>
2023-02-22 05:13:49 -0800

    task commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,11 @@
 #include "main.h"
 
 /**
- * _islower - checks for alphabet characters
+ * _isalpha - checks for alphabet characters
  * @c: the character to be evaluated
  * Return: 1 if c is an alphabet, else 0
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
