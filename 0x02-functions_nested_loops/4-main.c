c33d116da3aa67395031ad7b65885efd6474a3c7
eMode18 <ewagandi@gmail.com>
2023-02-22 03:43:52 -0800

    more tasks

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -2,7 +2,7 @@
 /**
  * main - check the code
  *
- * Retuen: Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the value absolute value of an integer
+ * @n: the int value to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
