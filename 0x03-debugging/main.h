3ab33368ce0b410e7394a32c8201d2aa23e4834d
eMode18 <ewagandi@gmail.com>
2023-02-23 18:56:50 -0800

    changes to main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 void postive_or_negative(int i);
-int largets_number(int a, int b, int c);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
