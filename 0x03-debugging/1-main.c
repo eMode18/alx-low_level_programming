b986d951d9fc50b258b4492d7463b6c54a9b5c50
eMode18 <ewagandi@gmail.com>
2023-02-23 18:51:40 -0800

    Debugging tasks

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - results in an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * 	putchar(i);
+	 * }
+	 */
+	printf("infinite loop avaoided! \\o/\n");
+
+	return (0);
+}
