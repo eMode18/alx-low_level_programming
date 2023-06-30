75fee597be203b9a10d7a7156dffc806c4d3540d
eMode18 <ewagandi@gmail.com>
2023-06-30 10:32:32 -0700

    task 2-add_node.c correction

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *append;
-	unsigned int length = 0;
+	unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	append = malloc(sizeof(list_t));
 	if (!append)
 		return (NULL);
 
 	append->str = strdup(str);
-	append->length = length;
+	append->len = len;
 	append->next = (*head);
 	(*head) = append;
 
