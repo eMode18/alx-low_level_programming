a8b13d42ac1b9105ec5cb0257c1feb90e024211b
eMode18 <ewagandi@gmail.com>
2023-07-10 05:50:26 -0700

    task 3cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -63,7 +63,7 @@ void close_file(int fdes)
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to, read_result, write_result;
-	char *ch;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -71,9 +71,9 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	ch = create_buffer(argv[2]);
+	buffer = create_buffer(argv[2]);
 	fd_from = open(argv[1], O_RDONLY);
-	read_result = read(fd_from, ch, 1024);
+	read_result = read(fd_from, buffer, 1024);
 	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
@@ -81,25 +81,25 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Unable to read from file %s\n", argv[1]);
-			free(ch);
+			free(buffer);
 			exit(98);
 		}
 
-		write_result = write(fd_to, ch, read_result);
+		write_result = write(fd_to, buffer, read_result);
 		if (fd_to == -1 || write_result == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Unable to write to file %s\n", argv[2]);
-			free(ch);
+			free(buffer);
 			exit(99);
 		}
 
-		read_result = read(fd_from, ch, 1024);
+		read_result = read(fd_from, buffer, 1024);
 		fd_to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (read_result > 0);
 
-	free(ch);
+	free(buffre);
 	close_file(fd_from);
 	close_file(fd_to);
 
