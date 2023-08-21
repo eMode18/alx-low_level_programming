64ddd935a12569a2a74fa9e8ac0cc16f2c4af8fc
eMode18 <ewagandi@gmail.com>
2023-08-21 11:59:25 -0700

    initial commit for task 0

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates the string pointed to by @src
+ *         including the terminant null byte, to the end of the string
+ *         pointed to by @dest.
+ * @dest: pointer to the string to be concatenated
+ * @src: source string that gets appended to @dest
+ * Return: pointer to the destination string @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
